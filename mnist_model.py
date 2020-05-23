bdee460c512444a47f3fe3e0f087e39b120687a1
shubh07092000 <59770773+shubh07092000@users.noreply.github.com>
2020-05-23 16:13:43 +0530

    code

---
+++ mnist_model.py
@@ -0,0 +1,77 @@
+from keras.datasets import mnist
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import RMSprop
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras_preprocessing.image import ImageDataGenerator
+from keras_preprocessing import image
+import PIL
+import keras
+from keras.utils import np_utils
+
+#datasets
+data = mnist.load_data('mymnist.db')
+train , test = data
+x_train, y_train = train
+x_test, y_test = test
+img_rows = x_train[0].shape[0]
+img_cols = x_train[1].shape[0]
+
+# Getting our date in the right 'shape' needed for Keras
+# We need to add a 4th dimenion to our date thereby changing our
+# Our original image shape of (60000,28,28) to (60000,28,28,1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+# store the shape of a single image
+input_shape = (img_rows, img_cols, 1)
+
+# change our image type to float32 data type
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Normalize our data by changing the range from (0 to 255) to (0 to 1)
+x_train /= 255
+x_test /= 255
+
+# Now we one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_test.shape[1]
+num_pixels = x_train.shape[1] * x_train.shape[2]
+
+#model compiling
+model = Sequential()
+model.add(Convolution2D(filters=16,
+                        kernel_size = (3,3),
+                        activation = 'relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(units = 512 ,input_dim = 28*28, activation = 'relu'))
+model.add(Dense(units = 256 ,input_dim = 28*28, activation = 'relu'))
+model.add(Dense(units = 128 ,input_dim = 28*28, activation = 'relu'))
+model.add(Dense(units = 10 , activation = 'softmax'))
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = keras.optimizers.Adadelta(),
+              metrics = ['accuracy'])
+# Training Parameters
+batch_size = 128
+epochs = 10
+
+history = model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+
+model.save("mnist1.h5")
+
+# Evaluate the performance of our trained model
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
